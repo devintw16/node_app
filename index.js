58bc909a0d888e5cc125a0800a4be9ff19e783f1
Devin Weeraratne <devintw16@gmail.com>
2024-04-19 23:35:04 +1000

    Simple web server hello node

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+// requestListener call back arrow function is defined inside  http.createServer
+// Two positional arguments req and res passed into callback function.
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
@@ -10,3 +12,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
+
